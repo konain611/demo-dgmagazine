5f0f1681a0191d705d1c79e98ee73f0b4ef5c2b3
Syed Konain Nasir <konain611@gmail.com>
2026-06-20 04:16:13 +0500

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import SubNavbar from "@/components/menus/Subnavbar";
 
 export default function Home() {
   return (
-   <div className="min-h-screen transition-colors">
+   <div className="min-h-[50vh] transition-colors">
    <SubNavbar />
    </div>
   );
