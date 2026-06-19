8b179988fdbfd1e07b810573da5492c159538559
Syed Konain Nasir <konain611@gmail.com>
2026-06-19 22:22:03 +0500

    added subfooter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Header from "@/components/menus/Header";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import Navbar from "@/components/menus/Navbar";
+import SubFooter from "@/components/footer/SubFooter";
+import Footer from "@/components/footer/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -35,6 +37,7 @@ export default function RootLayout({
           <Header />
           <Navbar />
           {children}
+          <SubFooter />
         </ThemeProvider>
       </body>
     </html>
---
+++ src/components/footer/SubFooter.tsx
@@ -0,0 +1,10 @@
+export default function SubFooter() {
+  return (
+    <footer className="bg-[#002448] py-2 text-gray-400">
+      <div className="container mx-auto flex items-center justify-between px-4 text-sm">
+        <p>© 2025 DG Magazine. All rights reserved.</p>
+        <p>DGMAGAZINE powered by DIGINFO & DGLABS (R&D Team).</p>
+      </div>
+    </footer>
+  )
+}
